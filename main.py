acb638f3ba49a180a7b77c004ca0ff0c3a62db56
William Richard <willster3021@gmail.com>
2014-03-15 15:58:08 -0400

    Call on the AI.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ Created on Mar 15, 2014
 @author: will
 '''
 from term2048.ui import start_game
+from ai.treeSearch import start_ai
 
 if __name__ == '__main__':
-    start_game()
+    #start_game()
+    start_ai()
